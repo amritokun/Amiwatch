6b89fac399ed64b55f575ed0a7fd8ebe1f112852
amritokun <bhabanidas431@gmail.com>
2025-09-04 22:19:56 +0530

    Refactor: Update Gradle and dependencies
    - Updated Gradle to version 8.13.
    - Updated Android Gradle Plugin to 8.11.1.
    - Updated NiceHttp library to version 0.4.4.
    - Enabled buildConfig in gradle.properties.
    - Configured repository management in settings.gradle.kts.
    - Adjusted Gradle JVM and bytecode target level.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
